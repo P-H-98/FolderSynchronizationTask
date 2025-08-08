e3ead0daa82e995ea69fb37f98f9d25542681ae7
P-H-98 <72311414+P-H-98@users.noreply.github.com>
2025-08-08 22:14:35 +0200

    :sparkles: Add SyncService class

---
+++ VeeamTest/SyncService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VeeamTest
+{
+    class SyncService
+    {
+        private readonly Logger _logger;
+
+        public SyncService(Logger logger)
+        {
+            _logger = logger;
+        }
+    }
+}
