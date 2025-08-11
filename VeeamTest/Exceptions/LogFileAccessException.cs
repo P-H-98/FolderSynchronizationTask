e1d2eddba6c5186d3909be01154638f7c6b93c40
P-H-98 <72311414+P-H-98@users.noreply.github.com>
2025-08-11 23:50:34 +0200

    :sparkles: Added new custom exceptions

---
+++ VeeamTest/Exceptions/LogFileAccessException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeeamTest.Exceptions
+{
+    /// <summary>
+    /// Custom exception thrown when accessing or creating the log file is not possible
+    /// </summary>
+    public class LogFileAccessException : Exception
+    {
+        public LogFileAccessException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- VeeamTest/Exceptions/SourceDirectoryNotFoundException.cs
+++ VeeamTest/Exceptions/SourceDirectoryNotFoundException.cs
@@ -9,7 +9,7 @@ namespace VeeamTest.Exceptions
     /// <summary>
     /// Custom exception thrown when source directory does not exist
     /// </summary>
-    internal class SourceDirectoryNotFoundException : Exception
+    public class SourceDirectoryNotFoundException : Exception
     {
         public SourceDirectoryNotFoundException(string path) : base($"Source directory does not exist: {path}")
         {
