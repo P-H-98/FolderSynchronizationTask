bb113a72b993b11172020e5c25f69987af7aa6e5
P-H-98 <72311414+P-H-98@users.noreply.github.com>
2025-08-13 00:39:14 +0200

    :white_check_mark: Added unit tests

---
+++ VeeamTest.Tests/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System;
+using System.IO;
+using VeeamTest.Exceptions;
+using Xunit;
+
+namespace VeeamTest.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void ValidateArguments_ThrowsException_WhenInvalidNumberOfArguments()
+        {
+            // Arrange
+            string[] args = new string[] { "sourcePath", "replicaPath" }; // Missing interval and log file
+            // Act & Assert
+            var ex = Assert.Throws<InvalidArgumentsException>(() => 
+                Program.ValidateArguments(new string[] {"only one argument"}));
+        }
+
+        [Fact]
+        public void ValidateArguments_ThrowsException_WhenIntervalIsInvalid()
+        {
+            // Arrange
+            string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+            string[] args = new string[] { tempDir, "replicaPath", "invalidInterval", Path.Combine(tempDir, "log.txt") };
+
+            // Act & Assert
+            var ex = Assert.Throws<InvalidArgumentsException>(() => 
+                Program.ValidateArguments(args));
+        }
+    }
+}
